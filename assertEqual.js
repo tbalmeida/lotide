3b10b60b0dd68d18f7faebb3ca7b84420bd1b19b
Thiago Almeida <tbalmeida@gmail.com>
2020-01-06 23:49:29 +0000

    With emoji

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,11 @@
+// Dependencies
+const emojic = require("emojic")
+    , colorIt = require("color-it")
+    ;
+  
 const assertEqual = function(actual, expected) {
-  actual === expected? console.log("Assertion Passed: " + actual + " === " + expected)
-    : console.log("Assertion Failed: " + actual + " !== " + expected );
+  actual === expected? console.log(emojic.heavyCheckMark + "  Assertion Passed: " + actual + " === " + expected)
+    : console.log(emojic.x + " Assertion Failed: " + actual + " !== " + expected );
   return null;
 };
 
