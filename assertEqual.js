af4a688a3e6ccd3147069a9c869e505f44f2dcce
Thiago Almeida <tbalmeida@gmail.com>
2020-01-07 00:26:00 +0000

    with Template Literals and Emojis

--- assertEqual.js
+++ assertEqual.js
@@ -4,9 +4,22 @@ const emojic = require("emojic")
     ;
   
 const assertEqual = function(actual, expected) {
-  actual === expected? console.log(emojic.heavyCheckMark + "  Assertion Passed: " + actual + " === " + expected)
-    : console.log(emojic.x + " Assertion Failed: " + actual + " !== " + expected );
-  return null;
+  let vResult = "";
+  let vSign = "";
+  let vEmoji = "";
+
+  if (actual === expected){
+    vResult = "  Assertion Passed";
+    vSign = "=";
+    vEmoji = emojic.heavyCheckMark;
+  } else {
+    vResult = " Assertion Failed";
+    vSign = "!";
+    vEmoji = emojic.x;
+  }
+  console.log(`${vEmoji}${vResult}: ${actual} ${vSign}== ${expected}`);
+
+    return null;
 };
 
 /* Tests
@@ -20,3 +33,11 @@ assertEqual("Star Wars", "Star Wars");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(20, 20.01);
+
+/* 1st version
+const assertEqual = function(actual, expected) {
+  actual === expected? console.log(emojic.heavyCheckMark + "  Assertion Passed: " + actual + " === " + expected)
+    : console.log(emojic.x + " Assertion Failed: " + actual + " !== " + expected );
+  return null;
+};
+*/
