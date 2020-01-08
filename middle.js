65d960d64e4a782a2c2716050743e10ca7f60f99
Thiago Almeida <tbalmeida@gmail.com>
2020-01-08 02:31:07 +0000

    Adjusted assertions

--- middle.js
+++ middle.js
@@ -74,9 +74,10 @@ const middle = function ( array ){
   return aRet;
 }
 
-console.log( middle([1]) ) // => []
-console.log( middle([1, 2]) ) // => []
-console.log( middle([1, 2, 3]) ) // => [2]
-console.log( middle([1, 2, 3, 4, 5]) ) // => [3]
-console.log( middle([1, 2, 3, 4]) ) // => [2, 3]
-console.log( middle([1, 2, 3, 4, 5, 6]) ) // => [3, 4]
+assertArraysEqual( middle([1]), [] );
+assertArraysEqual( middle([1]), [] );
+assertArraysEqual( middle([1, 2]), [] );
+assertArraysEqual( middle([1, 2, 3]), [2] );
+assertArraysEqual( middle([1, 2, 3, 4, 5]), [3] );
+assertArraysEqual( middle([1, 2, 3, 4]), [2, 3] );
+assertArraysEqual( middle([1, 2, 3, 4, 5, 6]), [3, 4] );
