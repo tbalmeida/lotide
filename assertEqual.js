413d1c8ac7613f6914e133addeddfb2046d58338
Thiago Almeida <tbalmeida@gmail.com>
2020-01-10 01:17:24 +0000

    Adjusted/improved

--- assertEqual.js
+++ assertEqual.js
@@ -1,23 +1,11 @@
 // Dependencies
-const emojic = require("emojic")
-  , colorIt = require("color-it")
-    ;
+const emojic = require("emojic"), colorIt = require("color-it");
   
 const assertEqual = function(actual, expected) {
-  let vResult = "";
-  let vSign = "";
-  let vEmoji = "";
 
-  if (actual === expected) {
-    vResult = "  Assertion Passed";
-    vSign = "=";
-    vEmoji = emojic.heavyCheckMark;
-  } else {
-    vResult = " Assertion Failed";
-    vSign = "!";
-    vEmoji = emojic.x;
-  }
-  console.log(`${vEmoji}${vResult}: ${actual} ${vSign}== ${expected}`);
+  actual === expected ?
+    console.log(emojic.heavyCheckMark + `  Assertion Passed: ${actual} === ${expected}`)
+    : console.log(emojic.x + ` Assertion Failed: ${actual} !== ${expected}`)
 
   return null;
 };
@@ -33,11 +21,3 @@ assertEqual("Star Wars", "Star Wars");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(20, 20.01);
-
-/* 1st version
-const assertEqual = function(actual, expected) {
-  actual === expected? console.log(emojic.heavyCheckMark + "  Assertion Passed: " + actual + " === " + expected)
-    : console.log(emojic.x + " Assertion Failed: " + actual + " !== " + expected );
-  return null;
-};
-*/
