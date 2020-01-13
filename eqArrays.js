d190e5abf349d616856a60aa01ea808621f87173
Thiago <tbalmeida@gmail.com>
2020-01-13 14:08:58 -0700

    Code refactored

--- eqArrays.js
+++ eqArrays.js
@@ -23,9 +23,5 @@ const eqArrays = function(arrA, arrB) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", ], ["1", "2", 3]), false);
 
 module.exports = eqArrays;
